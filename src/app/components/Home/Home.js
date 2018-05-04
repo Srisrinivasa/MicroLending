960aaef85992f6f4cf4fa5ad25e339b6f4632609
abhirocks550 <abhirocks550@gmail.com>
2018-05-04 19:31:05 +0530

    added background image with parallax

--- src/app/components/Home/Home.js
+++ src/app/components/Home/Home.js
@@ -4,20 +4,25 @@ import Lender from '../../container/Lender/Lender';
 
 const Home = () => {
     return (
-        <div className='container-fluid no-padding' id="landing-home">
-            <div className="row">
-                <div className="col-md-6">
-                    <Lender />
+        <div>
+            <div className='container-fluid' id="landing-home">
+                <div className="row no-margin">
+                    <div className="col-md-12 text-center bgColor lending-img">
+                        <h3 className="lending-heading">Lending Platform</h3>
+                        <p className="lending-paragraph">
+                            In this application Borrowers can find Lenders and vice-versa. 
+                            This is peer to peer connected architecture using BlockChain. 
+                        </p>
+                    </div>
                 </div>
-                <div className="col-md-6">
-                    <h3>Lending Platform</h3>
-                    <p>
-                        In this application Borrowers can find Lenders and vice-versa. 
-                        This is peer to peer connected architecture using BlockChain. 
-                    </p>
+                <br/>
+        </div>
+        <div className="row no-margin">
+                    <div className="col-md-12 bgColor">
+                    <Lender />                
                 </div>
             </div>
-        </div>
+      </div>
     );
   };
 
