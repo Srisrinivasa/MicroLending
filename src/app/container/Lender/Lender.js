7737b0c0fcc7235a55239cd691e6ea45950f1d01
abhirocks550 <abhirocks550@gmail.com>
2018-05-04 15:49:44 +0530

    Add react-bootstrap-table

--- src/app/container/Lender/Lender.js
+++ src/app/container/Lender/Lender.js
@@ -3,13 +3,45 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import './Lender.scss';
 
+// with es6
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'react-bootstrap-table/css/react-bootstrap-table.css';
+
+var products = [{
+    id: 1,
+    name: 'Abhijit',
+    price: 10000,
+    roi: 8.5,
+  },
+  {
+    id: 2,
+    name: 'Sathwik',
+    price: 5000,
+    roi: 8,
+  },
+  {
+    id: 3,
+    name: 'Kaushik',
+    price: 1000,
+    roi: 5,
+  },
+];
+
 class Lender extends React.Component {
   render() {
     return (
         <section className='container-fluid' id='LenderSection'>
                 <div className='row'>
                     <div className='col-md-12'>
-                        RenderList goes here
+                       <h3>Lenders list</h3>
+                        <BootstrapTable
+                            data={ products }
+                            pagination>
+                            <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+                            <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+                            <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+                            <TableHeaderColumn dataField='roi'>Roi</TableHeaderColumn>
+                        </BootstrapTable>
                     </div>
                 </div>
             </section>
